f9129733499189a49b33dafc72e81da2eb372093
bhuwanchaudhary715 <bhuwanchaudhary715@gmail.com>
2023-01-25 12:23:41 +0530

    Pascal_triangle.java

--- Pascal_triangle.java
+++ Pascal_triangle.java
@@ -17,14 +17,9 @@ public class Pascal_triangle {
         int ans[][]=new int[n][];
 
         for(int i=0;i<n;i++){
-            //ith row has i+1 column
-            /*
-                ans[0]=new int[1];
-                ans[1]=new int [2]
-             */
+             
             ans[i]=new int[i+1];
-            // 1st and last elements of every row is 1
-
+           
             ans[i][0]=ans[i][i]=1;
 
             for(int j=1;j<i;j++){
